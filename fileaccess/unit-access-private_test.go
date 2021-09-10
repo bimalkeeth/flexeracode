0ae6e1b7013fff3b4d5a4c6a6fab7cacf972ab34
bimal kaluarachchi <bimal.kaluarachchi@bidenergy.com>
2021-09-10 16:29:56 +1000

    feat:[test] implement functions for counter

--- fileaccess/unit-access-private_test.go
+++ fileaccess/unit-access-private_test.go
@@ -34,3 +34,27 @@ func Test_When_Prepare_Should_return_Error_When_File_With_Error(t *testing.T) {
 	assert.Error(t, err)
 
 }
+
+func Test_When_Prepare_Should_return_Success_With_Object(t *testing.T) {
+
+	fileAccess := fileAccess{fileName: "../sample-small-test.csv"}
+	stringFile := []string{"839", "8809", "374", "laptop", "Exported from System A", "839", "8809"}
+
+	entity, err := fileAccess.prepare(stringFile, nil)
+
+	t.Run("should not contain error", func(t *testing.T) {
+		assert.NoError(t, err)
+	})
+
+	t.Run("UserId should be equal to 8809", func(t *testing.T) {
+		assert.Equal(t, entity.UserID, "8809")
+	})
+
+	t.Run("ComputerId should be equal to 839", func(t *testing.T) {
+		assert.Equal(t, entity.ComputerID, "839")
+	})
+
+	t.Run("ComputerType should be equal to 'laptop'", func(t *testing.T) {
+		assert.Equal(t, entity.ComputerType, "laptop")
+	})
+}
